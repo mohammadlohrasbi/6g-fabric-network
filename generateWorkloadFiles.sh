20e0cfcfe4f7ecf08c9278494312843580f8f0c6
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-06-24 17:56:50 -0400

    Update generateWorkloadFiles.sh

--- generateWorkloadFiles.sh
+++ generateWorkloadFiles.sh
@@ -8,75 +8,71 @@ check_error() {
     fi
 }
 
-# مسیر ذخیره workload
-workload_dir="workload"
-mkdir -p "$workload_dir"
+echo "تولید فایل‌های workload..."
 
 # لیست قراردادها
-contracts=(
-    "GeoAssign" "GeoUpdate" "GeoHandover" "AuthUser" "AuthIoT" "ConnectUser" "ConnectIoT"
-    "BandwidthAlloc" "AuthAntenna" "RegisterUser" "RegisterIoT" "RevokeUser" "RevokeIoT"
-    "RoleAssign" "AccessControl" "AuditIdentity" "IoTBandwidth" "AntennaLoad" "ResourceRequest"
-    "QoSManage" "SpectrumShare" "PriorityAssign" "ResourceAudit" "LoadBalance" "DynamicAlloc"
-    "AntennaStatus" "IoTStatus" "NetworkPerf" "UserActivity" "FaultDetect" "IoTFault"
-    "TrafficMonitor" "ReportGenerate" "LatencyTrack" "EnergyMonitor" "Roaming" "SessionTrack"
-    "IoTSession" "Disconnect" "Billing" "TransactionLog" "ConnectionAudit" "DataEncrypt"
-    "IoTEncrypt" "AccessLog" "IntrusionDetect" "KeyManage" "PrivacyPolicy" "SecureChannel"
-    "AuditSecurity" "EnergyOptimize" "NetworkOptimize" "IoTAnalytics" "UserAnalytics"
-    "SecurityMonitor" "QuantumEncrypt" "MultiAntenna" "EdgeCompute" "IoTHealth" "NetworkHealth"
-    "DataIntegrity" "PolicyEnforce" "DynamicRouting" "BandwidthShare" "LatencyOptimize"
-    "FaultPredict" "IoTConfig" "UserConfig" "AntennaConfig" "PerformanceAudit" "SecurityAudit"
-    "DataAnalytics" "RealTimeMonitor"
+chaincodes=(
+  "GeoAssign" "GeoUpdate" "GeoHandover" "AuthUser" "AuthIoT" "ConnectUser" "ConnectIoT"
+  "BandwidthAlloc" "AuthAntenna" "RegisterUser" "RegisterIoT" "RevokeUser" "RevokeIoT"
+  "RoleAssign" "AccessControl" "AuditIdentity" "IoTBandwidth" "AntennaLoad" "ResourceRequest"
+  "QoSManage" "SpectrumShare" "PriorityAssign" "ResourceAudit" "LoadBalance" "DynamicAlloc"
+  "AntennaStatus" "IoTStatus" "NetworkPerf" "UserActivity" "FaultDetect" "IoTFault"
+  "TrafficMonitor" "ReportGenerate" "LatencyTrack" "EnergyMonitor" "Roaming" "SessionTrack"
+  "IoTSession" "Disconnect" "Billing" "TransactionLog" "ConnectionAudit" "DataEncrypt"
+  "IoTEncrypt" "AccessLog" "IntrusionDetect" "KeyManage" "PrivacyPolicy" "SecureChannel"
+  "AuditSecurity" "EnergyOptimize" "NetworkOptimize" "IoTAnalytics" "UserAnalytics"
+  "SecurityMonitor" "QuantumEncrypt" "MultiAntenna" "EdgeCompute" "IoTHealth" "NetworkHealth"
+  "DataIntegrity" "PolicyEnforce" "DynamicRouting" "BandwidthShare" "LatencyOptimize"
+  "FaultPredict" "IoTConfig" "UserConfig" "AntennaConfig" "PerformanceAudit" "SecurityAudit"
+  "DataAnalytics" "RealTimeMonitor"
 )
 
-# قالب workload
-template=$(cat << 'EOF'
+# ایجاد فایل workload برای هر قرارداد
+for cc in "${chaincodes[@]}"; do
+  cat > workload/${cc,,}.js << EOL
 const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
 
-class {{contractName}}Workload extends WorkloadModuleBase {
+class ${cc}Workload extends WorkloadModuleBase {
     constructor() {
         super();
-        this.contractId = '{{contractName}}';
-        this.contractVersion = 'v1';
-        this.entityCount = parseInt(process.env.ENTITY_COUNT || '1000');
+        this.entityCount = parseInt(process.env.ENTITY_COUNT) || 1000;
     }
 
     async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
         this.entities = [];
         for (let i = 0; i < this.entityCount; i++) {
-            const x = (Math.random() * 500).toFixed(2); // مختصات تصادفی بین 0 تا 500
-            const y = (Math.random() * 500).toFixed(2);
-            this.entities.push({ id: `entity_${workerIndex}_${i}`, x, y });
+            this.entities.push(\`entity\${workerIndex}_\${i}\`);
         }
     }
 
     async submitTransaction() {
-        const entity = this.entities[Math.floor(Math.random() * this.entities.length)];
-        const args = {
-            contractId: this.contractId,
-            contractVersion: this.contractVersion,
+        const entityID = this.entities[Math.floor(Math.random() * this.entities.length)];
+        const x = Math.random() * 500;
+        const y = Math.random() * 500;
+
+        const request = {
+            contractId: '${cc}',
             contractFunction: 'AssignEntity',
-            contractArguments: [entity.id, entity.x, entity.y],
+            contractArguments: [entityID, x.toString(), y.toString()],
             readOnly: false
         };
-        await this.sutAdapter.sendRequests(args);
+
+        await this.sutAdapter.sendRequests(request);
+    }
+
+    async cleanupWorkloadModule() {
+        // تمیزکاری
     }
 }
 
-async function createWorkloadModule() {
-    return new {{contractName}}Workload();
+function createWorkloadModule() {
+    return new ${cc}Workload();
 }
 
 module.exports.createWorkloadModule = createWorkloadModule;
-EOF
-)
-
-# تولید فایل‌های workload برای هر قرارداد
-for contract in "${contracts[@]}"; do
-    contract_lower=$(echo "$contract" | tr '[:upper:]' '[:lower:]')
-    echo "Processing workload: $contract_lower.js"
-    echo "$template" | sed "s#{{contractName}}#$contract#g" > "$workload_dir/$contract_lower.js"
-    check_error "تولید workload $contract_lower.js"
-    echo "Generated workload: $contract_lower.js"
+EOL
+  check_error "تولید workload $cc"
 done
+
+echo "فایل‌های workload با موفقیت تولید شدند!"
