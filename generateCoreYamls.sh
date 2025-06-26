1a19373df97f07a7739b37d846ea8fee53bffbf4
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-06-26 18:43:42 -0400

    Update generateCoreYamls.sh

--- generateCoreYamls.sh
+++ generateCoreYamls.sh
@@ -1,42 +1,21 @@
 #!/bin/bash
 
-# تابع بررسی خطا
-check_error() {
-    if [ $? -ne 0 ]; then
-        echo "خطا: $1"
-        exit 1
-    fi
-}
-
-echo "تولید فایل‌های core.yaml برای تمام سازمان‌ها..."
-
 for i in {1..10}; do
-  org="org${i}"
-  port=$((7051 + (i-1)*1000))
-  cat > core-org${i}.yaml << EOL
----
+  cat << EOF > core-org${i}.yaml
 peer:
-  id: cli-org${i}
-  networkId: 6gfabric
-  address: peer0.${org}.example.com:${port}
+  id: peer0.org${i}.example.com
+  networkId: fabric
+  listenAddress: 0.0.0.0:$((7051 + (i-1)*1000))
+  chaincodeListenAddress: 0.0.0.0:$((7052 + (i-1)*1000))
+  address: peer0.org${i}.example.com:$((7051 + (i-1)*1000))
   localMspId: Org${i}MSP
-  mspConfigPath: /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/${org}.example.com/users/Admin@${org}.example.com/msp
-  tls:
-    enabled: true
-    clientCert: /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/${org}.example.com/users/Admin@${org}.example.com/tls/client.crt
-    clientKey: /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/${org}.example.com/users/Admin@${org}.example.com/tls/client.key
-    rootcert: /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/${org}.example.com/peers/peer0.${org}.example.com/tls/ca.crt
+  mspConfigPath: /var/hyperledger/peer/msp
   gossip:
-    useLeaderElection: true
-    orgLeader: false
-    externalEndpoint: peer0.${org}.example.com:${port}
-  chaincodeListenAddress: 0.0.0.0:$((port+1))
-  chaincodeAddress: peer0.${org}.example.com:$((port+1))
-logging:
-  level: info
-fileSystemPath: /var/hyperledger/production
-EOL
-  check_error "تولید core-org${i}.yaml"
+    bootstrap: peer0.org${i}.example.com:$((7051 + (i-1)*1000))
+    externalEndpoint: peer0.org${i}.example.com:$((7051 + (i-1)*1000))
+  logging:
+    level: info
+EOF
 done
 
-echo "فایل‌های core.yaml با موفقیت تولید شدند!"
+echo "Generated core-org1.yaml to core-org10.yaml"
